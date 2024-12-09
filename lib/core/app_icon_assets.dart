959267cdaf2f608a8e1ca29e6c64866e4fafe1f0
ParimSsme <parima.ssme@gmail.com>
2024-12-09 15:35:37 +0430

    add icons

---
+++ lib/core/app_icon_assets.dart
@@ -0,0 +1,26 @@
+const _basePath = 'assets/icons';
+
+class AppIconAssets {
+  static const String notifications = '$_basePath/notifications.svg';
+  static const String search = '$_basePath/search.svg';
+  static const String favourite = '$_basePath/favourite.svg';
+  static const String favouriteFilled = '$_basePath/favourite_filled.svg';
+  static const String home = '$_basePath/home.svg';
+  static const String cart = '$_basePath/cart.svg';
+  static const String profile = '$_basePath/profile.svg';
+  static const String add = '$_basePath/add.svg';
+  static const String star = '$_basePath/star.svg';
+  static const String delete = '$_basePath/delete.svg';
+  static const String location = '$_basePath/location.svg';
+  static const String cash = '$_basePath/cash.svg';
+  static const String creditCard = '$_basePath/credit_card.svg';
+  static const String paypal = '$_basePath/paypal.svg';
+  static const String language = '$_basePath/language.svg';
+  static const String privacy = '$_basePath/privacy.svg';
+  static const String helpCenter = '$_basePath/help_center.svg';
+  static const String aboutUs = '$_basePath/about_us.svg';
+  static const String setting = '$_basePath/setting.svg';
+  static const String contact = '$_basePath/contact.svg';
+  static const String share = '$_basePath/share.svg';
+  static const String help = '$_basePath/help.svg';
+}
