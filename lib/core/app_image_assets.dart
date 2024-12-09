f2ef66bc78c57b243bc16c8c2081e15e7e5cf706
ParimSsme <parima.ssme@gmail.com>
2024-12-09 15:41:35 +0430

    add images

---
+++ lib/core/app_image_assets.dart
@@ -0,0 +1,20 @@
+const _basePath = 'assets/images';
+
+class AppImageAssets {
+  static const String userProfile = '$_basePath/user_profile.jpg';
+  static const String watch = '$_basePath/watch.jpg';
+  static const String nikeShoes = '$_basePath/nike_shoes.jpg';
+  static const String sliderBoy = '$_basePath/slider_boy.png';
+  static const String airpods = '$_basePath/airpods.jpg';
+  static const String LG_TV = '$_basePath/LG_tv.jpg';
+  static const String hoodie = '$_basePath/hoodie.jpg';
+  static const String jacket = '$_basePath/jacket.jpg';
+  static const String tShirt1 = '$_basePath/T_shirt1.jpg';
+  static const String tShirt2 = '$_basePath/T_shirt2.jpg';
+  static const String shoe1 = '$_basePath/shoe1.jpg';
+  static const String shoe2 = '$_basePath/shoe2.jpg';
+  static const String shoe3 = '$_basePath/shoe3.jpg';
+  static const String shoe4 = '$_basePath/shoe4.jpg';
+  static const String shoe5 = '$_basePath/shoe5.jpg';
+
+}
